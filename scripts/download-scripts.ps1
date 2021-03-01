c9f1cc9912f8cd63eff4ba76f1a99ee7f93ff3c0
Tobias Böker <tb@softwarepioniere.de>
2021-03-01 09:16:52 +0100

    update download

--- scripts/download-scripts.ps1
+++ scripts/download-scripts.ps1
@@ -6,26 +6,21 @@ param (
     'az-devops-extensions.ps1',
     'az-devops-org-security.ps1',
     'az-devops-projects.ps1'
-  ),
-  [switch] $delete
+  )
 )
 
 Write-Host '=============================='
 Write-Host 'Downloading Scripts'
 Write-Host '=============================='
 
-
 if (! (Test-Path $path)) {
   Write-Host "  Creating Directory $path"
   New-Item -Path $path -ItemType Directory
-} else {
-  if ($delete) {
-    Write-Host "  Recreating Directory $path"
-    Remove-Item -Path $path -Recurse -Force
-    New-Item -Path $path -ItemType Directory
-  }
+} else {  
+  Write-Host "  Recreating Directory $path"
+  Remove-Item -Path $path -Recurse -Force
+  New-Item -Path $path -ItemType Directory  
 }
-
 
 foreach ($s in $scripts) {
   Write-Host "  Downloading Script: $s"
