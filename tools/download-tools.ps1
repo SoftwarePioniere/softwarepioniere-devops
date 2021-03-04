fb6605fbecec8e701043a6a6ef2cb0c847760b4d
Tobias Böker <tb@softwarepioniere.de>
2021-03-04 13:49:50 +0100

    tools updates

---
+++ tools/download-tools.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+param (
+  $baseUrl = 'https://raw.githubusercontent.com/SoftwarePioniere/softwarepioniere-devops/main/scripts/',
+  $path   = 'downloaded-scripts',
+  $scripts = @(
+    'az-devops-extensions.ps1',
+    'az-devops-group-membership.ps1',
+    'az-devops-org-security.ps1',
+    'az-devops-pipelines.ps1',
+    'az-devops-project.ps1',
+    'az-devops-projects.ps1',
+    'az-devops-repos.ps1',
+    'az-devops-service-principals.ps1'
+  )
+)
+
+Write-Host '=============================='
+Write-Host 'Downloading Scripts'
+Write-Host '=============================='
+
+if (! (Test-Path $path)) {
+  Write-Host "  Creating Directory $path"
+  New-Item -Path $path -ItemType Directory
+} else {  
+  Write-Host "  Recreating Directory $path"
+  Remove-Item -Path $path -Recurse -Force
+  New-Item -Path $path -ItemType Directory  
+}
+
+foreach ($s in $scripts) {
+  Write-Host "  Downloading Script: $s"
+  $url = $baseUrl + $s
+  Write-Host "    Url: $url"
+  $output = Join-Path $path -ChildPath $s
+  Write-Host "    Output: $output"
+  $global:LASTEXITCODE = 0  
+  Invoke-WebRequest -Uri $url -OutFile $output
+  if ($LASTEXITCODE -ne 0) { throw 'error' }
+}
---
+++ tools/push-tools-update.ps1
@@ -0,0 +1,3 @@
+git add .
+git commit -m "tools updates"
+git push
