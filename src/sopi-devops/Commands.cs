eeb468ff4799dbcc90f1ca454cffe0960cd8fa4c
Tobias Boeker <tb@softwarepioniere.de>
2023-02-28 17:49:59 +0100

    move from system command line to spectre cli

---
+++ src/sopi-devops/Commands.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Spectre.Console.Cli;
+
+namespace SoftwarePioniere.DevOps;
+
+public class MyCommandSettings : CommandSettings
+{
+    // [CommandOption("--url")]
+    // [DefaultValue("localhost")]
+    // public string Url { get; set; }
+    //
+    // [CommandOption("--port")]
+    // [DefaultValue(2113)]
+    // public int Port { get; set; }
+
+    [CommandOption("--login-az-cli")]
+    [DefaultValue(false)]
+    public bool LoginAzCli { get; set; }
+}
+
+public abstract class MyCommandBase<T> : AsyncCommand<T> where T : CommandSettings
+{
+    // protected EventStoreSetupLogic CreateLogic()
+    // {
+    //     var logger = LoggerFactory.Create(builder => builder.AddSimpleConsole(options =>
+    //         {
+    //             options.IncludeScopes = true;
+    //             options.SingleLine = true;
+    //             options.TimestampFormat = "HH:mm:ss ";
+    //         }))
+    //         .CreateLogger(GetType());
+    //     var logic = new EventStoreSetupLogic(logger);
+    //     return logic;
+    // }
+}
+
+public class ShowSubscriptionsCommand : MyCommandBase<MyCommandSettings>
+{
+    private readonly SubscriptionWorker _subscriptionWorker;
+
+    public ShowSubscriptionsCommand(SubscriptionWorker subscriptionWorker)
+    {
+        _subscriptionWorker = subscriptionWorker;
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, MyCommandSettings settings)
+    {
+        await _subscriptionWorker.ListSubscriptions(settings.LoginAzCli);
+        return 0;
+    }
+}
--- src/sopi-devops/Worker.cs
+++ src/sopi-devops/Worker.cs
@@ -1,9 +1,10 @@
-﻿using System.CommandLine;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Spectre.Console.Cli;
 
 namespace SoftwarePioniere.DevOps;
 
@@ -13,119 +14,136 @@ public class Worker : BackgroundService
     private readonly IHostApplicationLifetime _appLifetime;
     private readonly SubscriptionWorker _subscriptionWorker;
     private readonly AzureAdWorker _azureAdWorker;
+    private readonly IServiceProvider _serviceProvider;
     private readonly WorkerParams _parms;
 
     public Worker(ILogger<Worker> logger,
         IHostApplicationLifetime appLifetime,
         IOptions<WorkerParams> options,
         SubscriptionWorker subscriptionWorker,
-        AzureAdWorker azureAdWorker)
+        AzureAdWorker azureAdWorker,
+        IServiceProvider serviceProvider)
     {
         _logger = logger;
         _appLifetime = appLifetime;
         _subscriptionWorker = subscriptionWorker;
         _azureAdWorker = azureAdWorker;
+        _serviceProvider = serviceProvider;
         _parms = options.Value;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogDebug("Run Worker");
-        var rootCommand = ConfigureApp();
-        await rootCommand.InvokeAsync(_parms.Args);
+        var app = new CommandApp(new TypeRegistrar2(_serviceProvider));
+
+        app.Configure(config =>
+        {
+            config.AddBranch("aad",
+                aad => { aad.AddCommand<ShowSubscriptionsCommand>("subscriptions"); });
+
+            // config.AddCommand<TestPasswordCommand>("test-password");
+            // config.AddCommand<EnableProjectionCommand>("enable-projection");
+            // config.AddCommand<GroupMemberCommand>("group-member");
+        });
+
+        await app.RunAsync(_parms.Args);
+
+        // _logger.LogDebug("Run Worker");
+        // var rootCommand = ConfigureApp();
+        // await rootCommand.InvokeAsync(_parms.Args);
 
         _appLifetime.StopApplication();
     }
 
-    RootCommand ConfigureApp()
-    {
-        var rootCommand = new RootCommand();
-        AppendAddCommand(rootCommand);
+    // RootCommand ConfigureApp()
+    // {
+    //     var rootCommand = new RootCommand();
+    //     AppendAddCommand(rootCommand);
+    //
+    //     {
+    //         var cmd = new Command("subscriptions");
+    //         rootCommand.Add(cmd);
+    //
+    //         var loginAzCliOption = new Option<bool>("--login-az-cli",
+    //             "Login from az cli Service Principial, not from Environment vars");
+    //
+    //         var list = new Command("list")
+    //         {
+    //             loginAzCliOption
+    //         };
+    //         list.SetHandler<bool>((loginAzCli) => _subscriptionWorker.ListSubscriptions(loginAzCli), loginAzCliOption);
+    //         cmd.Add(list);
+    //     }
+    //
+    //     return rootCommand;
+    // }
 
-        {
-            var cmd = new Command("subscriptions");
-            rootCommand.Add(cmd);
-
-            var loginAzCliOption = new Option<bool>("--login-az-cli",
-                "Login from az cli Service Principial, not from Environment vars");
-
-            var list = new Command("list")
-            {
-                loginAzCliOption
-            };
-            list.SetHandler((bool loginAzCli) => _subscriptionWorker.ListSubscriptions(loginAzCli), loginAzCliOption);
-            cmd.Add(list);
-        }
-
-        return rootCommand;
-    }
-
-    void AppendAddCommand(Command rootCommand)
-    {
-        var cmd = new Command("aad");
-        rootCommand.Add(cmd);
-
-        var tempdir = @"c:\temp\aad-dev";
-
-        var loginAzCliOption = new Option<bool>("--login-az-cli",
-            "Login from az cli Service Principial, not from Environment vars");
-        var dataDirOption = new Option<string>("--data-dir",
-            () => tempdir,
-            "The Directory which contains the data files. Defaults to Current Directory");
-        var defaultPassordOption = new Option<string>("--default-password",
-            () => "Password01",
-            "The Default Passwort on User Creation");
-        var userFilePatternOption = new Option<string>("--user-file-pattern",
-            () => "benutzer*.json",
-            "Pattern to read the User Files");
-        var groupFilePatternOption = new Option<string>("--group-file-pattern",
-            () => "gruppen*.json",
-            "Pattern to read the Group Files");
-        var dryRunOption = new Option<bool>("--dry-run",
-            "Just dry run. No changes");
-
-        var show = new Command("show", "Parse and list the Data Files and Load From AAD")
-        {
-            loginAzCliOption,
-            dataDirOption,
-            userFilePatternOption,
-            groupFilePatternOption
-        };
-        cmd.Add(show);
-        show.SetHandler((bool loginAzCli, string dataDir, string userFilePattern,
-                string groupFilePattern)
-            => _azureAdWorker.ShowAadUsersAndGroups(loginAzCli, dataDir, userFilePattern, groupFilePattern));
-
-        var export = new Command("export", "Export existing AAD")
-        {
-            loginAzCliOption,
-            dataDirOption,
-            userFilePatternOption,
-            groupFilePatternOption
-        };
-        cmd.Add(export);
-        export.SetHandler((bool loginAzCli, string dataDir, string userFilePattern,
-                string groupFilePattern) =>
-            _azureAdWorker.ExportAadUsersAndGroups(loginAzCli, dataDir, userFilePattern, groupFilePattern));
-
-        var deploy = new Command("deploy", "Deploy the AAD")
-        {
-            loginAzCliOption,
-            dataDirOption,
-            defaultPassordOption,
-            userFilePatternOption,
-            groupFilePatternOption,
-            dryRunOption
-        };
-        cmd.Add(deploy);
-        deploy.SetHandler((bool loginAzCli, string dataDir, string defaultPassword,
-                string userFilePattern, string groupFilePattern, bool dryRun) =>
-            _azureAdWorker
-                .DeployAadUsersAndGroups(loginAzCli,
-                    dataDir,
-                    defaultPassword,
-                    userFilePattern,
-                    groupFilePattern,
-                    dryRun));
-    }
+    // void AppendAddCommand(Command rootCommand)
+    // {
+    //     var cmd = new Command("aad");
+    //     rootCommand.Add(cmd);
+    //
+    //     var tempdir = @"c:\temp\aad-dev";
+    //
+    //     var loginAzCliOption = new Option<bool>("--login-az-cli",
+    //         () => false,
+    //         "Login from az cli Service Principial, not from Environment vars");
+    //     var dataDirOption = new Option<string>("--data-dir",
+    //         () => tempdir,
+    //         "The Directory which contains the data files. Defaults to Current Directory");
+    //     var defaultPassordOption = new Option<string>("--default-password",
+    //         () => "Password01",
+    //         "The Default Passwort on User Creation");
+    //     var userFilePatternOption = new Option<string>("--user-file-pattern",
+    //         () => "benutzer*.json",
+    //         "Pattern to read the User Files");
+    //     var groupFilePatternOption = new Option<string>("--group-file-pattern",
+    //         () => "gruppen*.json",
+    //         "Pattern to read the Group Files");
+    //     var dryRunOption = new Option<bool>("--dry-run",
+    //         () => false,
+    //         "Just dry run. No changes");
+    //
+    //     var show = new Command("show", "Parse and list the Data Files and Load From AAD")
+    //     {
+    //         loginAzCliOption,
+    //         dataDirOption,
+    //         userFilePatternOption,
+    //         groupFilePatternOption
+    //     };
+    //     cmd.Add(show);
+    //     show.SetHandler<bool, string, string, string>((loginAzCli, dataDir, userFilePattern, groupFilePattern)
+    //         => _azureAdWorker.ShowAadUsersAndGroups(loginAzCli, dataDir, userFilePattern, groupFilePattern));
+    //
+    //     var export = new Command("export", "Export existing AAD")
+    //     {
+    //         loginAzCliOption,
+    //         dataDirOption,
+    //         userFilePatternOption,
+    //         groupFilePatternOption
+    //     };
+    //     cmd.Add(export);
+    //     export.SetHandler<bool, string, string, string>((loginAzCli, dataDir, userFilePattern, groupFilePattern) =>
+    //         _azureAdWorker.ExportAadUsersAndGroups(loginAzCli, dataDir, userFilePattern, groupFilePattern));
+    //
+    //     var deploy = new Command("deploy", "Deploy the AAD")
+    //     {
+    //         loginAzCliOption,
+    //         dataDirOption,
+    //         defaultPassordOption,
+    //         userFilePatternOption,
+    //         groupFilePatternOption,
+    //         dryRunOption
+    //     };
+    //     cmd.Add(deploy);
+    //     deploy.SetHandler<bool, string, string, string, string, bool>(
+    //         (loginAzCli, dataDir, defaultPassword, userFilePattern, groupFilePattern, dryRun) =>
+    //             _azureAdWorker
+    //                 .DeployAadUsersAndGroups(loginAzCli,
+    //                     dataDir,
+    //                     defaultPassword,
+    //                     userFilePattern,
+    //                     groupFilePattern,
+    //                     dryRun));
+    // }
 }
